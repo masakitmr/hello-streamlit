e629d099f9434e0a29932383c30a1a95293d1a4a
masakitmr <126246013+masakitmr@users.noreply.github.com>
2025-05-21 15:40:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.title("Hello World")
-st.write("これは Streamlit Cloud 上で動作する超シンプルなアプリです。")
+st.title("簡単な診断アプリ")
+name = st.text_input("お名前を入力してください：")
+age = st.number_input("年齢を入力してください：", min_value=0)
+
+if st.button("診断する"):
+    if age >= 20:
+        st.success(f"{name}さんは成人です。")
+    else:
+        st.info(f"{name}さんは未成年です。")
